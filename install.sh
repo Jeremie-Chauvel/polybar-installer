3e9da7c590d3c9eef8d495d8a6a7afbe2d7767a9
jeremiec <Jeremie-Chauvel@users.noreply.github.com>
2020-10-24 12:44:06 +0200

    Fix install

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ ln -sr ./config "$polybar_config_dir"
 # install dependencies
 sudo apt install --yes build-essential git cmake cmake-data pkg-config python3-sphinx \
   libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev \
-  libxcb-composite0-dev python-xcbgen xcb-proto libxcb-image0-dev \
+  libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev \
   libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev \
   libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev \
   libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev
@@ -30,13 +30,6 @@ cd polybar
 git checkout 3.4.2 # version
 
 # compile polybar
-set +e
-hash pyenv
-result_status="$?"
-set -e
-if [[ "$result_status" == 0 ]]; then
-  pyenv local system
-fi
 set +euo
 ./build.sh --i3 --pulseaudio --network --curl --auto
 set -euo
